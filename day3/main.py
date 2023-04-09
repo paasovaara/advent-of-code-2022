a6db3a883b46cdf7380e9140b4506b11174d8dd7
Markus Paasovaara <markus.paasovaara@gmail.com>
2023-04-09 15:22:01 +0300

    Solve part 2 of day 3

--- day3/main.py
+++ day3/main.py
@@ -1,4 +1,16 @@
-# This solution is 100% provided by chat-gpt 🤯
+# Part1 solution is 100% provided by chat-gpt 🤯, the Part2 is also assembled from the chat-gpt output
+
+def find_common_item_type(items) -> str:
+    # Convert the list of lists to a set of sets for efficient intersection.
+    item_sets = [set(item_list) for item_list in items]
+    
+    # Find the intersection of all three sets.
+    common_items = set.intersection(*item_sets)
+    
+    if not common_items:
+        return None
+    return common_items.pop()
+
 
 def item_priority(item: str):
     if not isinstance(item, str) or len(item) != 1 or not item.isascii() or not item.isalpha():
@@ -10,16 +22,28 @@ def item_priority(item: str):
     else:
         return ord(item) - 38
 
+def solve_part1 (rucksacks): 
+    total_priority = 0
+    for rucksack in rucksacks:
+        half_len = len(rucksack) // 2
+        first_half = rucksack[:half_len]
+        second_half = rucksack[half_len:]
+        common_items = set(first_half) & set(second_half)
+        common_priority = sum([item_priority(item) for item in common_items])
+        total_priority += common_priority
+
+    print("Part 1 answer: ", total_priority)
+
+def solve_part2 (groups):
+    group_common_items = [find_common_item_type(items) for items in groups]
+    group_priorities = [item_priority(item) for item in group_common_items]
+    total_priority = sum(group_priorities)
+    print("Part 2 answer: ", total_priority)
+
+
 with open('input.txt', 'r') as f:
     rucksacks = [line.strip() for line in f.readlines()]
+    groups = [rucksacks[i:i+3] for i in range(0, len(rucksacks), 3)]
 
-total_priority = 0
-for rucksack in rucksacks:
-    half_len = len(rucksack) // 2
-    first_half = rucksack[:half_len]
-    second_half = rucksack[half_len:]
-    common_items = set(first_half) & set(second_half)
-    common_priority = sum([item_priority(item) for item in common_items])
-    total_priority += common_priority
-
-print(total_priority)
+solve_part1(rucksacks)
+solve_part2(groups)
